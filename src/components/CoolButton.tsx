df4a93b7f8d44d64dcc137ae73a53878830dd1da
MonsteRico <boilermonster0530@yahoo.com>
2022-09-06 22:16:36 -0400

    Home page complete

--- src/components/CoolButton.tsx
+++ src/components/CoolButton.tsx
@@ -3,13 +3,17 @@ import React from "react";
 interface CoolButtonProps {
 	onClick?: () => void;
 	children?: React.ReactNode;
+	className?: string;
 }
 
-export default function CoolButton({ onClick, children }: CoolButtonProps) {
+export default function CoolButton({ onClick, children, className }: CoolButtonProps) {
 	return (
 		<button
 			onClick={onClick}
-			className="transition ease-in-out duration-300 p-3 bg-violet-500 rounded hover:bg-violet-700"
+			className={
+				"transition ease-in-out duration-300 p-3 bg-violet-500 rounded hover:bg-violet-700" +
+				(className ? " " + className : "")
+			}
 		>
 			{children}
 		</button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 import GameCover from "../components/GameCover";
 import CoolButton from "../components/CoolButton";
 import { useRef } from "react";
+import useWindowDimensions from "../utils/useWindowDimensions";
 const Home: NextPage = () => {
 	const { data: session } = useSession();
 	let text: string = "World";
@@ -15,9 +16,34 @@ const Home: NextPage = () => {
 		text = session?.user?.name as string;
 		steamId = session?.steamId as string;
 	}
-	const hello = trpc.useQuery(["hello.world", { text }]);
+	const { width, height } = useWindowDimensions();
 	// create an array with 30 elements
-	const arr = Array.from(Array(27).keys());
+	let numberColumns = 0;
+	let numberRows = 0;
+	const arr = [];
+	if (width && height) {
+		// Round number columns to a multiple of 3
+		numberRows = Math.ceil(height / 350);
+		numberColumns = Math.ceil(width / 200);
+		console.log(height / 450);
+		if (numberColumns % 3 !== 0) {
+			numberColumns = Math.ceil(numberColumns / 3) * 3;
+			if (numberColumns > 9) {
+				numberColumns = 9;
+			}
+		}
+		if (numberRows % 3 !== 0) {
+			numberRows = Math.ceil(numberRows / 3) * 3;
+			if (numberRows > 4) {
+				numberRows = 4;
+			}
+		}
+		for (let i = 0; i < numberRows * numberColumns; i++) {
+			arr.push(i);
+		}
+		console.log("numberRows", numberRows);
+		console.log("numberColumns", numberColumns);
+	}
 	const randomAppIdsQuery = trpc.useQuery(["randomGame.getRandomGames", { number: arr.length }], {
 		refetchOnWindowFocus: false,
 	});
@@ -30,21 +56,19 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<NavBar></NavBar>
-			<div className="grid grid-cols-9 grid-rows-3 overflow-hidden z-1">
+			<div
+				className={`grid grid-cols-${numberColumns} col-span-12 row-span-6 grid-rows-${numberRows} overflow-hidden z-1`}
+			>
 				{randomAppIdsQuery.data &&
 					arr.map((i) => {
 						const middleRow = i > 8 && i < 17;
 						return (
 							<motion.div
 								key={i}
-								initial={middleRow ? { opacity: 0, x: 100 } : { opacity: 0, x: -100 }}
-								animate={middleRow ? { opacity: 1, x: 0 } : { opacity: 1, x: 0 }}
-								transition={
-									middleRow
-										? { duration: 0.25, delay: (8 - (i % 9)) * 0.1 }
-										: { duration: 0.25, delay: (i % 9) * 0.1 }
-								}
-								className="p-4"
+								initial={{ opacity: 0, rotateY: 180 }}
+								animate={{ opacity: 1, rotateY: 0 }}
+								transition={{ duration: 0.25, delay: (i % numberColumns) * 0.1 }}
+								className="col-span-1 row-span-1 p-5"
 								style={{ width: "200px", height: "300px" }}
 							>
 								<GameCover alt="Game Cover" appId={randomAppIdsQuery.data[i]} installed />
@@ -74,15 +98,40 @@ const Home: NextPage = () => {
 			</div>
 			<div
 				style={{ top: "6.75em" }}
-				className="bg-black z-2 grid grid-cols-9 grid-rows-3 absolute left-0 h-full w-full bg-opacity-70 border-b-2 border-violet-500"
+				className={`bg-black z-2 grid grid-cols-${numberColumns} grid-rows-${numberRows} absolute left-0 h-full w-full bg-opacity-70 border-b-2 border-violet-500`}
 			>
-				{" "}
+				<div style={{ height: "300px", width: "200px" }}></div>
 			</div>
 
-			<main className="container mx-auto flex flex-col items-center justify-center p-4">
-				<div ref={scrollRef} className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-					{hello.data ? <p>{hello.data.greeting}</p> : <p>Loading...</p>}
-				</div>
+			<main className="container mx-auto flex flex-col items-center justify-center align-center p-4">
+				<motion.div
+					animate={{ opacity: 1, y: 0 }}
+					initial={{ opacity: 0, y: 100 }}
+					transition={{ duration: 0.5, delay: 0.5 }}
+					ref={scrollRef}
+					className="mb-64 mt-32 flex flex-col justify-center items-center mx-auto w-full"
+				>
+					<h1 className="text-center text-7xl mb-5 px-5 pb-5 font-bold border-violet-500 border-b-4">
+						How does it work?
+					</h1>
+					<p className="text-center text-4xl">
+						This website lets you log in with Discord, checks your Steam ID from your Discord Connections,
+						and lets you see the games you and your friends have in common. That&apos;s all there is too it!
+						No more arguing over what game to play or going through each persons owned games to find what
+						you all can play together!
+					</p>
+					<div className="mt-10 flex flex-row jusitfy-center items-center mx-auto align-center">
+						<h1 className="text-center p-5 text-5xl font-bold text-violet-500">You ready to try?</h1>
+						<CoolButton
+							className="mt-2"
+							onClick={() => {
+								signIn("discord");
+							}}
+						>
+							<p className="font-bold">Click here to sign in!</p>
+						</CoolButton>
+					</div>
+				</motion.div>
 			</main>
 		</>
 	);
